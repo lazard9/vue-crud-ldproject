4d1735cbe63cd8809478c6e4be41103f8395b076
Lazar Dacic <lazar.dacic@gmail.com>
2025-06-15 23:56:55 +0200

    feat: add DestinationForm to edit and add new destination pages

--- src/views/DestinationAdd.vue
+++ src/views/DestinationAdd.vue
@@ -1,11 +1,16 @@
 <script setup>
 import HeroSection from '../components/HeroSection.vue';
+import DestinationForm from '../components/DestinationForm.vue';
 </script>
 
 <template>
     <main class="space-y-20 mb-20">
         <section class="max-w-full mx-auto">
             <HeroSection title="Add a Destination" />
+        </section>
+
+        <section class="max-w-2xl mx-auto px-4">
+            <DestinationForm />
         </section>
     </main>
 </template>
--- src/views/DestinationEdit.vue
+++ src/views/DestinationEdit.vue
@@ -1,1 +1,31 @@
-<template>tere</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import HeroSection from '../components/HeroSection.vue';
+import DestinationForm from '../components/DestinationForm.vue';
+
+const route = useRoute();
+const destination = ref(null);
+
+onMounted(async () => {
+    const res = await fetch(`/api/destinations?slug=${route.params.slug}`);
+    const data = await res.json();
+    destination.value = data[0]; // Assuming slug is unique
+});
+</script>
+
+<template>
+    <main class="space-y-20 mb-20" v-if="destination">
+        <section class="max-w-full mx-auto">
+            <HeroSection title="Edit Destination" />
+        </section>
+
+        <section class="max-w-2xl mx-auto px-4">
+            <h2 v-if="destination.title"
+                class="text-3xl md:text-4xl text-center font-bold text-neutral-900 dark:text-white mb-12">{{
+                destination.title }}</h2>
+
+            <DestinationForm :existingData="destination" />
+        </section>
+    </main>
+</template>
